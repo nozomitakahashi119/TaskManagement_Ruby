4803525f67da66c77ba75b8abd5b008ced8480dd
nozomi.takahashi <hgoopde624@yahoo.co.jp>
2020-03-01 18:25:15 +0000

    user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  root to: 'tasks#index'
+  get 'toppages/index'
+  get 'users/index'
+  get 'users/show'
+  get 'users/new'
+  get 'users/create'
+  root to: 'toppages#index' #メインページの設定
   
+  get 'signup', to:'users#new'
   resources :tasks
+  resources :users, only: [:index, :show, :new, :create]
 end
