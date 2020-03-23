a2333b84861dce2b7df1fa0c3643c61bc77dc551
nozomi.takahashi <hgoopde624@yahoo.co.jp>
2020-03-23 09:01:40 +0000

    2nd times update

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,7 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in#, only: [:index, :show]
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  #before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :correct_user,only: [:show, :edit, :update, :destroy]
   include SessionsHelper
   
   def index
@@ -29,9 +30,13 @@ class TasksController < ApplicationController
   end
   
   def edit
+    @task = Task.find(params[:id])
   end
   
   def update
+    
+    @task = Task.find(params[:id])
+    
     if @task.update(task_params)
       flash[:success] = 'Task は正常に更新されました'
       redirect_to @task
@@ -42,6 +47,7 @@ class TasksController < ApplicationController
   end
   
   def destroy
+    @task = Task.find(params[:id])
     @task.destroy
     
     flash[:success] = 'Task は正常に削除されました'
@@ -50,12 +56,22 @@ class TasksController < ApplicationController
   
   private
   
+=begin
   def set_task
     @task = Task.find(params[:id])
-  end
+=end
   
+ 
   # Strong Parameter
   def task_params
     params.require(:task).permit(:content, :status)
   end
+  
+  def correct_user #本人かどうか確認、違うとすれば一覧に遷移
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+  end
+  
 end
