dd0832007cfe84347ed24d652b2bd67c52046dec
nozomi.takahashi <hgoopde624@yahoo.co.jp>
2020-03-27 06:19:12 +0000

    4th times update

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -29,9 +29,9 @@ class TasksController < ApplicationController
     end
   end
   
-  #def edit
-    #@task = Task.find(params[:id])
-  #end
+  def edit
+    @task = Task.find(params[:id])
+  end
   
   def update
     
